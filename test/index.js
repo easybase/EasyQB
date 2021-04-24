ac96825e0e413f73bdecec4d6662eeddffbd2b0f
Dilan <dio4@pitt.edu>
2021-04-24 13:49:06 -0400

    removed string ops

--- test/index.js
+++ test/index.js
@@ -12,27 +12,13 @@ const table2 = easyqb('table2');
 const { e: e2 } = table2;
 
 console.log(
-  table1.return(exp.eq('aa', 'fantasy')).return('123').where(
+  table1.return(exp.add())).return('123').where(
     exp.and(
       exp.eq`first_name`('Mohammed'),
       exp.gt('age', '30'),
-      exp.gte`age`('30')
+      exp.gte`age`('30'),
+      exp.lteAny("asdf", "cc")
     )
   ).groupBy('hello').one(),
-  
-  table2.set({ firstName: 'Robert', nickname: 'Rob' })
-    .where({ firstName: 'Matt' })
-    .all(),
-
-  table2.insert({ firstName: 'Shallan', lastName: 'Davar' }).all(),
-
-    table1.delete().where(
-      exp.and(
-        exp.lt('col', 12),
-        exp.gte('col', '70')
-      )
-    ).one(),
-
-    table1.set({ available: false }).one(),
 
 );
